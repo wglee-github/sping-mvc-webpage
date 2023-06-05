b68bd6f4608dc2670aa204bed72078e9ccbc0ce4
wglee <amd0223@gmail.com>
2023-06-05 23:57:38 +0900

    spring mvc webpage frist commit

---
+++ src/main/java/hello/itemservice/SpringMvcWebpageApplication.java
@@ -0,0 +1,13 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcWebpageApplication.class, args);
+	}
+
+}
